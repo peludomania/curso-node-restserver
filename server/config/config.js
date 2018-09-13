5a843c099d6d33a84c7b3c53d42396ddf6038a2a
Valera <alberto.valera@stay-app.com>
2018-09-13 14:55:31 +0200

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+
+
+
+
+// ==========================================
+//  Puerto, si no está definido, ponemos el 3000
+// ==========================================
+
+process.env.PORT = process.env.PORT || 3000;
